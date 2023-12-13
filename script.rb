43fd879af6182dd04e7acedc9083cfd43025c58c
Jarrett <contact@jarrettperkins.me>
2023-12-13 11:26:25 -0600

    removed pry

--- script.rb
+++ script.rb
@@ -1,10 +1,6 @@
-require 'pry-byebug'
-
 def isogram?(string)
   original_length = string.length
   string_array = string.downcase.split
-
-  binding.pry
 
   unique_length = string_array.uniq.length
   original_length == unique_length
